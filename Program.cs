27515cc473e4b0dae3daa18f93e99bbb37aa5ada
Viktoriia <savchukv2006@gmail.com>
2026-06-01 19:09:22 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using UniversityTasksDbFirstApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<UniversityTasksDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .EnableSensitiveDataLogging(false));
 
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
